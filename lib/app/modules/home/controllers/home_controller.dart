b45d164bded4fd396bb9d5d38d26184adadf5400
Budiman Rasyid <brasyid15@gmail.com>
2021-12-14 14:56:14 +0700

    Membuat logic untuk checkInternet

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,9 +1,24 @@
 import 'package:get/get.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
 
 class HomeController extends GetxController {
-  //TODO: Implement HomeController
+  final _hasInternet = false.obs;
+  final _text = ''.obs;
+
+  set hasInternet(val) => _hasInternet.value = val;
+
+  set text(val) => _text.value = val;
+
+  get hasInternet => _hasInternet.value;
+
+  get text => _text.value;
+
+  Future<bool> checkInternet() async {
+    return await InternetConnectionChecker().hasConnection;
+  }
 
   final count = 0.obs;
+
   @override
   void onInit() {
     super.onInit();
@@ -16,5 +31,6 @@ class HomeController extends GetxController {
 
   @override
   void onClose() {}
+
   void increment() => count.value++;
 }
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:overlay_support/overlay_support.dart';
 
 import '../controllers/home_controller.dart';
 
@@ -9,13 +10,30 @@ class HomeView extends GetView<HomeController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('HomeView'),
+        title: Text('Check Connectivity'),
         centerTitle: true,
       ),
       body: Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
+        child: ElevatedButton(
+          child: Text('Check Internet'),
+          onPressed: () async {
+            controller.hasInternet = await controller.checkInternet();
+
+            final color = controller.hasInternet ? Colors.green : Colors.red;
+
+            controller.text =
+                controller.hasInternet ? 'Internet' : 'No Internet';
+
+            showSimpleNotification(
+              Text(
+                '${controller.text}',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              background: color,
+            );
+          },
         ),
       ),
     );
